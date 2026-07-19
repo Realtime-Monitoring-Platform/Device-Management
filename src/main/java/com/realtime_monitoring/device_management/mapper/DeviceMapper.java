61893eb571708feb2c1ef37cd71cd1c8a546d3f2
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-19 12:56:22 +0100

    add DeviceMapper

---
+++ src/main/java/com/realtime_monitoring/device_management/mapper/DeviceMapper.java
@@ -0,0 +1,19 @@
+package com.realtime_monitoring.device_management.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+import com.realtime_monitoring.device_management.dto.CreateDeviecRequest;
+import com.realtime_monitoring.device_management.dto.DeviceResponse;
+import com.realtime_monitoring.device_management.entity.Device;
+
+@Mapper(
+    componentModel = MappingConstants.ComponentModel.SPRING
+)
+public interface DeviceMapper {
+    Device toEntity(CreateDeviecRequest createDeviceRequest);
+
+    DeviceResponse toResponse(Device device);
+
+    
+}
