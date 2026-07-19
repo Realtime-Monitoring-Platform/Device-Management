0c9c0c1113e2014027365a54c7553e6c6fbca5d2
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-19 12:13:53 +0100

    add dependecies

---
+++ src/main/java/com/realtime_monitoring/device_management/entity/Device.java
@@ -0,0 +1,15 @@
+package com.realtime_monitoring.device_management.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Table(name = "device")
+@AllArgsConstructor
+@NoArgsConstructor
+public class Device {
+    
+}
