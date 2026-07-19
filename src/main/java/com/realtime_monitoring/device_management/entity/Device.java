3a3aa9aa28dfb42f03c11ea8806acf9700c05f4c
ChedlyRebai <chedly.rebai123@gmail.com>
2026-07-19 12:29:37 +0100

    feat: update Device entitys

--- src/main/java/com/realtime_monitoring/device_management/entity/Device.java
+++ src/main/java/com/realtime_monitoring/device_management/entity/Device.java
@@ -1,5 +1,18 @@
 package com.realtime_monitoring.device_management.entity;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,5 +24,42 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Device {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(nullable = false)
+    private UUID tenantId;
+
+    private UUID teamId;
+
+    private UUID assignedUserId;
+
+    @Column(nullable = false)
+    private String deviceName;
+
+    private String model;
+
+    private String manufacturer;
+
+    @Column(nullable = false)
+    private String hostname;
+
+    private String ipAddress;
+
+    private String macAddress;
+
+    private String location;
+
+    // @Enumerated(EnumType.STRING)
+    // private DeviceStatus status;
+
+    private LocalDateTime lastSeen;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 }
