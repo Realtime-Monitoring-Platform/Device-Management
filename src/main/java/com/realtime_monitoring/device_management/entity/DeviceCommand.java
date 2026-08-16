f24e3e7acd715f4d4becbaaba04b54083c32c8c2
ChedlyRebai <chedly.rebai123@gmail.com>
2026-08-16 14:40:48 +0100

    add DeviceCommand updates

--- src/main/java/com/realtime_monitoring/device_management/entity/DeviceCommand.java
+++ src/main/java/com/realtime_monitoring/device_management/entity/DeviceCommand.java
@@ -7,9 +7,13 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumeratedValue;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
@@ -19,20 +23,24 @@ import jakarta.persistence.Column;
 @Entity
 @Getter
 @Setter
+@Table
 @RequiredArgsConstructor
 public class DeviceCommand {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
     private UUID deviceId;
 
     private UUID tenantId;
 
+    @Enumerated(EnumType.STRING)
+    private CommandStatus status;
 
-    
     private UUID userId;
-
+    @Column(columnDefinition = "TEXT")
+    private String stdout;
+    
     private String command;
 
     @CreationTimestamp
